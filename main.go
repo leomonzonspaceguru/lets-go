1d5a28c2a69c463baa83cf5ec8c182f72e9a55e7
Monzón <leonardo.monzon@spaceguru.com>
2024-09-18 00:11:16 -0300

    leomonzondev

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hola desde func home"))
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id,err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display",id)
+	w.Write([]byte(msg))
+
 }
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/snippet/view/{id}", snippetView)
-	http.HandleFunc("/snippet/create", snippetCreate)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("El server corre on :4000")
 
-	err := http.ListenAndServe(":4000", nil)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
